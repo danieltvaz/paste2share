d418679e4418a5e0f3d23c1de2b08a318ab46fea
danieltvaz <danieltostes@live.com>
2024-08-02 11:48:58 -0300

    feat: sync connection messages

--- frontend/src/app/paste/[socketid]/page.tsx
+++ frontend/src/app/paste/[socketid]/page.tsx
@@ -4,13 +4,14 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 import Button from "@/components/client/button";
 import TextArea from "@/components/client/text-area";
-import { socket } from "@/services/socket";
-import { useRouter } from "next/navigation";
+import { useRouter as navigationRouter } from "next/navigation";
+import { socket } from "@/services/socket/socket";
 
 export default function Home() {
-  const router = useRouter();
+  const router = navigationRouter();
   const [connectionId, setConnectionId] = useState<string>("");
   const socketRef = useRef(socket);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     socketRef.current.connect();
@@ -18,8 +19,8 @@ export default function Home() {
     socketRef.current.on("connectionId", (id) => {
       setConnectionId(id);
 
-      socketRef.current.on(id, (message: any) => {
-        console.log("mensagem recebida:", message);
+      socketRef.current.on(id, (message: { from: string; message: string }) => {
+        setInputValue(message.message);
       });
     });
 
@@ -34,12 +35,27 @@ export default function Home() {
     }
   }
 
+  function copyToClipboard() {
+    navigator.clipboard.writeText(location.protocol + "//" + window.location.host + window.location.pathname);
+  }
+
+  function disconnect() {
+    socketRef.current.disconnect();
+    if (socketRef.current.disconnected) {
+      router.replace(location.protocol + "//" + window.location.host);
+    }
+  }
+
   return (
     <section className="flex flex-col items-center gap-4">
-      <TextArea onChange={handleChange} />
+      <TextArea onChange={handleChange} value={inputValue} />
       <div className="flex gap-2">
-        <Button variant="blue">Copy link to clipboard</Button>
-        <Button variant="red">Close this share</Button>
+        <Button variant="blue" onClick={copyToClipboard}>
+          Copy link to clipboard
+        </Button>
+        <Button variant="red" onClick={disconnect}>
+          Close this share (forever)
+        </Button>
       </div>
     </section>
   );
