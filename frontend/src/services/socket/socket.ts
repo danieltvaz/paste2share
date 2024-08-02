6bec9c52216bb9907892d4350ec589ba0588e7c6
danieltvaz <danieltostes@live.com>
2024-08-02 11:48:42 -0300

    chore: move module

--- frontend/src/services/socket/socket.ts
+++ frontend/src/services/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3001";
+const URL = "http://192.168.68.108:3001";
 
 export const socket = io(URL, { autoConnect: false, transports: ["websocket"] });
