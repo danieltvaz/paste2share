59d96f6c96d38e5fee4620c77ea563a7c873e731
danieltvaz <danieltostes@live.com>
2025-06-02 20:06:39 -0300

    feat: add adsense

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,12 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <AdsenseScript /> */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4064829886079582"
-          crossOrigin="anonymous"
-        />
+          crossOrigin="anonymous"></script>
       </head>
       <body>
         <Header />
