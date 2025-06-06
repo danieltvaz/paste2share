49d0521e68ffb906edf6491986673204150bee11
danieltvaz <danieltostes@live.com>
2025-06-05 21:36:19 -0300

    chore: remove unnused adsense

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,12 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4064829886079582"
-          crossOrigin="anonymous"></script>
-      </head>
+      <head></head>
       <body>
         <Header />
         <main>{children}</main>
