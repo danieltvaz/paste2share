4e3a05b0b533789044888a0ed1aeac4ca89767e3
danieltvaz <danieltostes@live.com>
2025-06-05 21:52:38 -0300

    feat: add width prop

--- frontend/src/components/client/button/index.tsx
+++ frontend/src/components/client/button/index.tsx
@@ -2,9 +2,12 @@ import "./button.css";
 
 import { ComponentProps } from "react";
 
-export default function Button({ variant, ...props }: ComponentProps<"button"> & { variant: "blue" | "red" }) {
+export default function Button({
+  variant,
+  ...props
+}: ComponentProps<"button"> & { variant: "blue" | "red"; width?: string }) {
   return (
-    <button {...props} className={variant === "blue" ? "button-blue" : "button-red"}>
+    <button {...props} className={variant === "blue" ? "button-blue" : "button-red"} style={{ width: props.width }}>
       {props.children}
     </button>
   );
