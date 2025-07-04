1122c30b003102bc3cea5d39b524ef2a27014f9c
danieltvaz <danieltostes@live.com>
2025-07-04 19:08:50 -0300

    chore: rebuild image every run

--- run-all-podman.sh
+++ run-all-podman.sh
@@ -4,19 +4,11 @@ image_exists() {
   podman image exists "$1"
 }
 
-if image_exists paste2share-backend; then
-  echo "Image paste2share-backend already exists."
-else
-  echo "Building paste2share-backend image..."
-  podman build -t paste2share-backend -f backend/Dockerfile ./backend
-fi
+echo "Building paste2share-backend image..."
+podman build -t paste2share-backend -f backend/Dockerfile ./backend
 
-if image_exists paste2share-frontend; then
-  echo "Image paste2share-frontend already exists."
-else
-  echo "Building paste2share-frontend image..."
-  podman build -t paste2share-frontend -f frontend/Dockerfile ./frontend
-fi
+echo "Building paste2share-frontend image..."
+podman build -t paste2share-frontend -f frontend/Dockerfile ./frontend
 
 echo "Running backend container..."
 podman run -d -it --rm -p 3001:3001 -v $(pwd)/backend:/app paste2share-backend
