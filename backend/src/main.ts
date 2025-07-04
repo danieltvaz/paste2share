ab9f521da3c1ad6e4c9dcff22823a616f94e4d0e
danieltvaz <danieltostes@live.com>
2025-07-04 20:24:01 -0300

    fix: manual add response headers for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,19 @@
+import { NextFunction, Response } from 'express';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? 'https://paste2share.danieltostes.dev'
-        : '*',
-    methods: 'GET',
+  app.enableCors();
+
+  app.use((_, res: Response, next: NextFunction) => {
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://paste2share-api.danieltostes.dev',
+    );
+    next();
   });
 
   await app.listen(
