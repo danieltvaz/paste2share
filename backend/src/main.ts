b023edb7d144acc88e106f7fae2a50d1f48de03e
danieltvaz <danieltostes@live.com>
2025-07-04 19:00:59 -0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,16 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://paste2share.danieltostes.dev'
+        : '*',
+    methods: 'GET',
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
