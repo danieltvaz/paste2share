271db6b1d45ee11ff7cab33be926ab7f1a1f8e59
danieltvaz <danieltostes@live.com>
2025-07-04 16:22:12 -0300

    chore: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3001);
 }
 bootstrap();
