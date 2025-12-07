ea3ce26a08a72c7a7eac749e442244ff67516eb2
danieltvaz <danieltostes@live.com>
2025-12-07 07:40:07 -0300

    feat: add config module for read env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebsocketModule } from './websocket/websocket.module';
 import { QrcodeModule } from './qrcode/qrcode.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WebsocketModule, QrcodeModule],
+  imports: [WebsocketModule, QrcodeModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
